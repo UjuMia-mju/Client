ab6c340f4bc335fe0ce844b46d167763b4e96993
DoodleOfSun <92295388+DoodleOfSun@users.noreply.github.com>
2026-02-05 21:46:21 +0900

    모든 움직이는 객체들의 부모 클래스 추가

---
+++ Assets/Scripts/AllMovingObjects/MovingObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    public Rigidbody rb { get; protected set; }
+    protected Vector3 moveAmount;
+    public float walkSpeed = 10;
+    public float jumpForce = 5;
+
+    protected LayerMask groundMask;
+    protected LayerMask wallMask;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected virtual void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    protected virtual void Moving(Vector3 movDir)
+    {
+        Vector3 targetMoveAmount = movDir * walkSpeed;
+        moveAmount = Vector3.MoveTowards(moveAmount, targetMoveAmount, walkSpeed);
+        rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
+    }
+
+    protected virtual void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+}
