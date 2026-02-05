708b76461f4961d666f82cb1acd2db601dd144c3
DoodleOfSun <92295388+DoodleOfSun@users.noreply.github.com>
2026-02-05 21:50:09 +0900

    플레이어 카메라 스크립트 추가

---
+++ Assets/Scripts/AllMovingObjects/Player/PlayerTPCamera.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerTPCamera : MonoBehaviour
+{
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+
+    public Transform playerMesh;
+
+    public float distance = 5.0f;
+
+    private float currentX = 0.0f;
+    private float currentY = 45.0f;
+
+
+    private GameObject player;
+    private PlayerGravityController pgr;
+
+    private void Start()
+    {
+        player = transform.parent.gameObject;
+        pgr = FindFirstObjectByType<PlayerGravityController>();
+
+    }
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X");
+        currentY += -Input.GetAxis("Mouse Y");
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    private void LateUpdate()
+    {
+        // 행성 중심을 향한 중력 방향 계산
+        Vector3 gravityUp = (playerMesh.position - pgr.planet.transform.position).normalized;
+
+        //// Vector3.up 벡터를 gravityUp으로 회전시키는데 필요한 쿼터니언을 생성
+        Quaternion gravityRotation = Quaternion.FromToRotation(Vector3.up, gravityUp);
+
+        //// 마우스 X 입력을 gravityUp 축으로 하는 회전 쿼터니언 생성
+        Quaternion yaw = Quaternion.AngleAxis(currentX, gravityUp);
+
+        //// 마우스 Y 입력을 gravityUp에 수직인 축(오른쪽 방향)으로 하는 회전 쿼터니언 생성
+        Vector3 right = yaw * gravityRotation * Vector3.right;
+
+        //// 위에서 생성한 right 벡터를 회전축으로 삼고, currentY(마우스 Y 입력)만큼 회전시키는 쿼터니언을 생성
+        Quaternion pitch = Quaternion.AngleAxis(currentY, right);
+
+        //// 최종 카메라 회전 쿼터니언 계산
+        Quaternion finalRotation = pitch * yaw * gravityRotation;
+
+        //// 카메라 위치 설정 및 회전
+        Vector3 dir = finalRotation * Vector3.back * distance;
+        this.transform.position = playerMesh.position + dir;
+        this.transform.rotation = Quaternion.LookRotation(-dir, gravityUp);
+    }
+}
