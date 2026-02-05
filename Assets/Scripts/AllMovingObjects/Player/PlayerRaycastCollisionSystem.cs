70ef5e03125dcd286a8ba288851348483abad007
DoodleOfSun <92295388+DoodleOfSun@users.noreply.github.com>
2026-02-05 21:49:54 +0900

    플레이어가 레이캐스트로 무언가를 감지하는 스크립트 추가

---
+++ Assets/Scripts/AllMovingObjects/Player/PlayerRaycastCollisionSystem.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class PlayerRaycastCollisionSystem : MonoBehaviour
+{
+    private const float RAY_LENGTH = 2.1f;
+
+    private bool isGrounded = true;
+
+    public bool CollisionDetectWithRaycast(Vector3 dirData, LayerMask wallMask)
+    {
+        Vector3 rayTargetDir = transform.TransformDirection(dirData);
+
+        Ray ray = new Ray(this.transform.position, rayTargetDir);
+        RaycastHit hit;
+
+
+        Debug.DrawLine(ray.origin, ray.origin + ray.direction * (RAY_LENGTH), Color.blue);
+
+        if (Physics.Raycast(ray, out hit, RAY_LENGTH, wallMask))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void GroundDetectingWithRaycast(LayerMask groundMask)
+    {
+        // 행성 방향으로 레이캐스트 발사 - 레이캐스트
+        Vector3 origin = transform.position + transform.up * 0.5f;
+        Ray ray = new Ray(origin, -transform.up);
+
+        RaycastHit hit;
+
+        Debug.DrawLine(ray.origin, ray.origin + ray.direction * (1.1f), Color.red);
+
+        // 발이 땅에 닿았을 때를 감지
+        if (Physics.Raycast(ray, out hit, 1.1f, groundMask))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    public bool GetIsGrounded()
+    {
+        return isGrounded;
+    }   
+}
