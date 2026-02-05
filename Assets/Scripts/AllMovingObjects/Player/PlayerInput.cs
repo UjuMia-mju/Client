7adf813ea397e1d97e9b4f9f06a649f247701ec1
DoodleOfSun <92295388+DoodleOfSun@users.noreply.github.com>
2026-02-05 21:49:18 +0900

    플레이어 입력 스크립트 추가

---
+++ Assets/Scripts/AllMovingObjects/Player/PlayerInput.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float axisX { get; private set; } = 0;
+    public float axisY { get; private set; } = 0;
+    public Vector3 axisResultDir { get; private set; } = Vector3.zero;
+
+
+    private bool isJumping = false;
+
+    public void InputProcess()
+    {
+        axisX = Input.GetAxisRaw("Horizontal");
+        axisY = Input.GetAxisRaw("Vertical");
+        axisResultDir = new Vector3(axisX, 0, axisY).normalized;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+    }
+
+    public bool GetIsJumping()
+    {
+        return isJumping;
+    }
+
+    public void MakeIsJumpingFalse()
+    {
+        isJumping = false;
+    }
+}
