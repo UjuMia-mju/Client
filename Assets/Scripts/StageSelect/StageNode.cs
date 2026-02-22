65c359ff329b455b350b3573ba22a1f628b7de89
Park Jiwoo <jiwoo4237@naver.com>
2026-02-22 16:30:11 +0900

    EventSystem 인터페이스로 변경

--- Assets/Scripts/StageSelect/StageNode.cs
+++ Assets/Scripts/StageSelect/StageNode.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(SphereCollider))]
-public class StageNode : MonoBehaviour
+public class StageNode : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     [Header("Orbit (공전) Settings")]
     public Transform orbitCenter; 
@@ -20,7 +21,7 @@ public class StageNode : MonoBehaviour
     [SerializeField] private GameObject stagePanelPrefab; 
     
     private MenuManager _menuManager;
-    private StageManager _stageManager; // 매니저 참조 추가
+    private StageManager _stageManager;
     private Vector3 _originalScale;
     private Vector3 _targetScale;
 
@@ -33,47 +34,37 @@ public class StageNode : MonoBehaviour
         _targetScale = _originalScale;
     }
 
-    // StageManager가 호출하는 이동 로직
     public void UpdateMovement(float deltaTime)
     {
-        if (orbitCenter != null)
-        {
-            transform.RotateAround(orbitCenter.position, orbitAxis, orbitSpeed * deltaTime);
-        }
+        if (orbitCenter != null) transform.RotateAround(orbitCenter.position, orbitAxis, orbitSpeed * deltaTime);
         transform.Rotate(spinAxis, spinSpeed * deltaTime, Space.Self);
     }
 
-    // StageManager가 호출하는 크기 변환(Hover) 로직
     public void UpdateScale(float deltaTime)
     {
         if (transform.localScale != _targetScale)
-        {
             transform.localScale = Vector3.Lerp(transform.localScale, _targetScale, deltaTime * hoverTransitionSpeed);
-        }
     }
 
-    private void OnMouseEnter()
+    // 마우스가 올라갔을 때
+    public void OnPointerEnter(PointerEventData eventData)
     {
         _targetScale = _originalScale * hoverScaleMultiplier;
     }
 
-    private void OnMouseExit()
+    // 마우스가 벗어났을 때
+    public void OnPointerExit(PointerEventData eventData)
     {
         _targetScale = _originalScale;
     }
 
-    private void OnMouseDown()
+    // 클릭했을 때
+    public void OnPointerClick(PointerEventData eventData)
     {
         if (_menuManager != null && stagePanelPrefab != null)
         {
-            // 1. 카메라 줌인 & 패널 팝업
             _menuManager.StartZoomSequence(this.transform, stagePanelPrefab);
-            
-            // 2. 패널이 열렸으므로 전체 행성의 움직임 일시 정지 (어지러움 방지)
-            if (_stageManager != null)
-            {
-                _stageManager.SetMovementPause(true);
-            }
+            if (_stageManager != null) _stageManager.SetMovementPause(true);
         }
     }
 }
