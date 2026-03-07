d5d6839cff79249ad07a2c632e805b874ab9db06
Hongryeol In <phbti924@gmail.com>
2026-03-07 15:16:20 +0900

    PacketId 수정

--- Assets/Scripts/Server/PacketId.cs
+++ Assets/Scripts/Server/PacketId.cs
@@ -13,5 +13,32 @@ namespace Protocol
         PKT_S_PLAYER_LIST = 1008,
         PKT_S_PLAYER_ENTER = 1009,
         PKT_S_PLAYER_LEAVE = 1010,
+
+        // 방 관련
+        PKT_C_CREATE_ROOM = 1011,
+        PKT_S_CREATE_ROOM = 1012,
+        PKT_C_ROOM_LIST = 1013,
+        PKT_S_ROOM_LIST = 1014,
+        PKT_C_ENTER_ROOM = 1015,
+        PKT_S_ENTER_ROOM = 1016,
+        PKT_C_LEAVE_ROOM = 1017,
+        PKT_S_LEAVE_ROOM = 1018,
+
+        // 초대 관련
+        PKT_C_INVITE_PLAYER = 1019,
+        PKT_S_INVITE_PLAYER = 1020,
+        PKT_S_INVITE_NOTIFICATION = 1021,
+        PKT_C_INVITE_RESPONSE = 1022,
+        PKT_S_INVITE_RESPONSE = 1023,
+
+        // 방 멤버 알림
+        PKT_S_ROOM_MEMBER_ENTER = 1024,
+        PKT_S_ROOM_MEMBER_LEAVE = 1025,
+
+        // 준비 / 게임 시작
+        PKT_C_READY = 1026,
+        PKT_S_READY = 1027,
+        PKT_C_START_ROOM = 1028,
+        PKT_S_START_ROOM = 1029,
     }
 }
