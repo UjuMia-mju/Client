682b1ed405ee9ea85fb8f1276a5fbd8b61825cd1
DoodleOfSun <92295388+DoodleOfSun@users.noreply.github.com>
2026-03-20 17:53:58 +0900

    요청대로 타이머 기능을 추가. UI는 구현되지 않음.

---
+++ Assets/Scripts/GameRule/GameRuleManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameRuleManager : MonoBehaviour
+{
+    private float timerDuration = 10f;
+    private float remainingTime;
+
+    void Start()
+    {
+        StartCoroutine(StartTimer());
+    }
+
+    IEnumerator StartTimer()
+    {
+        remainingTime = timerDuration;
+
+        while (remainingTime > 0)
+        {
+            Debug.Log("남은 시간: " + remainingTime + "초");
+            yield return new WaitForSeconds(1f); // 1초마다 감소
+            remainingTime -= 1f;
+        }
+
+        Debug.Log("타이머 종료!");
+        OnTimerEnd();
+    }
+
+    void OnTimerEnd()
+    {
+        // 타이머가 끝났을 때 실행할 로직
+        Debug.Log("게임 규칙 실행!");
+    }
+}
