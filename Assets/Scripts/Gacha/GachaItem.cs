b436afb3e9d9abea8cea186642dd01f375edd27f
Hongryeol In <phbti924@gmail.com>
2026-03-27 20:31:52 +0900

    GachaItem.cs: 로컬 확률용 weight 필드를 제거

--- Assets/Scripts/Gacha/GachaItem.cs
+++ Assets/Scripts/Gacha/GachaItem.cs
@@ -7,5 +7,4 @@ public class GachaItem : ScriptableObject
     [TextArea(2, 5)] public string descriptionText;
     public Sprite icon;
     public ItemRarity rarity;
-    public int weight;    // 확률 가중치 (높을수록 잘 나옴) 모든 아이템의 가중치 합이 100이 될 필요는 없음.
 }
