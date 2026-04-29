cec26e4f93d6a2a75c053e50721873f3ff7e34ba
DoodleOfSun <92295388+DoodleOfSun@users.noreply.github.com>
2026-04-29 23:13:49 +0900

    WalkableCollider가 법선 벡터 조기화 가능하도록 수정

--- Assets/Scripts/AllMovingObjects/Player/Player.cs
+++ Assets/Scripts/AllMovingObjects/Player/Player.cs
@@ -187,7 +187,7 @@ public class Player : MovingObject
             KeyFInteract();
         }
 
-        GetGroundNormal(groundMask);
+        GetGroundNormal(groundMask | walkable);
         SphereTriggerFunc();
     }
 
